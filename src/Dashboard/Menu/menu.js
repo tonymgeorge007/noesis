7b77fb25ed4fd55a8543d54877d167a9534db481
Tony George <tonymgeorge007@gmail.com>
2019-12-16 20:00:04 +0530

    icons updated

--- src/Dashboard/Menu/menu.js
+++ src/Dashboard/Menu/menu.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
-import './menu.css'
-import home from '../../Images/home.png';
+import './menu.css';
+import home from '../../Images/icon_home.svg';
+import noise from '../../Images/Group 4.svg';
+import sensors from '../../Images/Icon metro-wrench.svg';
+import help from '../../Images/icon_help-center.svg';
+import setting from '../../Images/icon_setting.svg';
+import logo from '../../Images/NOESIS_LOGO.png';
+
+
+
 
 class Menu extends Component {
   render() {
@@ -15,24 +23,24 @@ class Menu extends Component {
                     <span className="menu-span"><img src={home} className="menu-icon"/> <p  className="menu-list">HOME</p></span>
                     </li>
                     <li  className="row">
-                    <span className="menu-span"><img src={home} className="menu-icon"/> <p  className="menu-list">NOISE LEVELS</p></span>
+                    <span className="menu-span"><img src={noise} className="menu-icon"/> <p  className="menu-list">NOISE LEVELS</p></span>
                     </li>
                     <li  className="row">
-                    <span className="menu-span"><img src={home} className="menu-icon"/> <p  className="menu-list">SENSORS</p></span>
+                    <span className="menu-span"><img src={sensors} className="menu-icon"/> <p  className="menu-list">SENSORS</p></span>
                     </li>
                     <li  className="row">
                     <span className="menu-span"><img src={home} className="menu-icon"/> <p  className="menu-list">HEATMAPS</p></span>
                     </li>
                     <li  className="row">
-                    <span className="menu-span"><img src={home} className="menu-icon"/> <p  className="menu-list">HELP</p></span>
+                    <span className="menu-span"><img src={help} className="menu-icon"/> <p  className="menu-list">HELP</p></span>
                     </li>
                     <li  className="row">
-                    <span className="menu-span"><img src={home} className="menu-icon"/> <p  className="menu-list">SETTINGS</p></span>
+                    <span className="menu-span"><img src={setting} className="menu-icon"/> <p  className="menu-list">SETTINGS</p></span>
                     </li>
                     
                 </ul>
             </div>
-
+            <img src={logo} className="logo-icon"/>
             <h3 className="menu-bottom">NOESIS</h3>
     </div>
     );
