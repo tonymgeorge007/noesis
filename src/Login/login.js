3294308fbfca1b35389115ba79c0b05b53327650
Tony George <tonymgeorge007@gmail.com>
2019-12-21 22:08:38 +0530

    Menu UI fixes

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,20 +1,70 @@
 import React, { Component } from 'react';
 import './login.css';
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import {Redirect} from 'react-router-dom';
 
 class Login extends Component {
+
+  state = {
+    name:'',
+    password:'',
+    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoibm9lc2lzX3dlYiJ9.2oQCiI1OR8q_nSGEudKSt5X3KgJ0QRi_MVsVk0-7uyw'
+  }
+
+handleChangename(e){
+    this.setState({name: e.target.value})
+ }
+
+ handleChangepass(e){
+  this.setState({password: e.target.value})
+}
+
+sumbitLogin() {
+
+    const user = {
+      p_username: this.state.name,
+      p_seckey: this.state.password
+    };
+console.log("user",user);
+
+    const instance = axios.create({
+      baseURL: 'http://34.90.114.171:3000',
+      headers: {
+        Authorization: "Bearer " + this.state.token,
+        "Content-Type": "application/json"
+      }
+    });
+
+  instance.post(`/rpc/userauth`, user  )
+  .then(res => {
+    let login_response = res.data;
+    if (login_response[0].p_result.displayname != null)
+    {
+      this.props.history.push('/dashboard');
+    }
+    else{
+      alert(login_response[0].p_result.error_text)
+    }
+  })
+}
+
   render() {
     return (
       <div className="loginscreen">
-
       <div className="loginform">
       <div className="row paddingtop20">
-
         <div className="col-md-4">
         <p>Username</p>
         </div>
 
         <div className="col-md-8">
-        <input type="text"  className="floatleft" id="username" name="username" />
+        <input type="text"  className="floatleft" id="username" name="username"  value={this.state.name} onChange={(e) => this.handleChangename(e)} />
         </div>
       </div>
 
@@ -24,12 +74,12 @@ class Login extends Component {
        </div>
 
        <div className="col-md-8">
-        <input type="text"  className="floatleft" id="password" name="password" />
+        <input type="text"  className="floatleft" id="password" name="password"  value={this.state.password} onChange={(e) => this.handleChangepass(e)} />
         </div>
        </div>
 
        <div className="row">
-       <button type="button" class="btn btn-success loginbutton">Login</button>
+       <button type="button" class="btn btn-success loginbutton" onClick={(e) => this.sumbitLogin(e)}>Login</button>
        </div>
 
       </div>
