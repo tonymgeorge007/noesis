2f169d6baa87db755ed2f598f5f48008ba8b32ee
Tony George <tonymgeorge007@gmail.com>
2020-02-10 14:16:44 +0530

    map updated

--- src/Dashboard/Dashboardview/mapviewleaf.js
+++ src/Dashboard/Dashboardview/mapviewleaf.js
@@ -1,34 +1,33 @@
 import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
-export default class SimpleExample extends React.Component {
+type State = {
+  lat: number,
+  lng: number,
+  zoom: number,
+}
 
-  constructor() {
-    super();
-    this.state = {
+export default class SimpleExample extends Component<{}, State> {
+  state = {
       lat: 51.996256,
       lng: 5.974894,
       zoom: 15,
-    };
   }
 
   render() {
-    const position = [this.state.lat, this.state.lng];
+    const position = [this.state.lat, this.state.lng]
     return (
-      <div id="container">
       <Map center={position} zoom={this.state.zoom}>
         <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={position}>
           <Popup>
-            <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
+            A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
       </Map>
-      </div>
-    );
-
+    )
   }
 }
